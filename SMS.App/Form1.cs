199e819c37cef3380bb1ccef1c03de04758db762
CpE Laboratory <CpE Laboratory@BE216-PC11>
2025-03-27 14:20:49 +0800

    Add project files.

---
+++ SMS.App/Form1.cs
@@ -0,0 +1,53 @@
+using SMS.Domain;
+
+namespace SMS.App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonShow_Click(sender, e);
+            //createStudent(1, "Shakira", "sha@kira.com");
+        }
+        
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            
+            createStudent(2, "Joan Dela Cruz", "joan@delacruz.com");
+            
+        }
+
+        private void createStudent(int id, string name, string email)
+        {
+            var student = new Student
+            {
+                StudentId = id,
+                StudentName = name,
+                Email = email,
+                DateAdded = DateTime.Now,
+                AddedBy = "Admin"
+            };
+            textBoxEmail.Text = student.Email;
+            textBoxID.Text = student.StudentId.ToString();
+            textBoxName.Text = student.StudentName;
+
+            //labelAddedInformation.Text = "Added By: " + student.AddedBy + " " + student.DateAdded;
+            labelAddedInformation.Text = $"Added By: {student.AddedBy} {student.DateAdded}";
+        }
+    }
+}
---
+++ SMS.Infastructure/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SMS.Infastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
