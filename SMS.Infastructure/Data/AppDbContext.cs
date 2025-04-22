9b16e2f667e6927327ac0747f3636e02b8bc5b5f
PJBayta <pjbayta@gmail.com>
2025-04-22 14:49:40 +0800

    Infrastructure update

--- SMS.Infastructure/Data/AppDbContext.cs
+++ SMS.Infastructure/Data/AppDbContext.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SMS.Domain;
 
 namespace SMS.Infastructure.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
+        public AppDbContext() : base() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=db_students;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
